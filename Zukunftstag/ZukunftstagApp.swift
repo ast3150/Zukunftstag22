426850017033accd94f94089cdba5228acf38473
Alain Stulz <alain@appswithlove.com>
2022-11-10 13:54:23 +0100

    Initial Commit

---
+++ Zukunftstag/ZukunftstagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZukunftstagApp.swift
+//  Zukunftstag
+//
+//  Created by Alain Stulz on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ZukunftstagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
